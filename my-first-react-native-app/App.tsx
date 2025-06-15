017dd7a985099aa244e1bc51ef7cbc7911e2d33b
Macbook Air M4 <inoue.shinichi.1800@gmail.com>
2025-06-15 14:52:31 +0900

    [U] layout

--- my-first-react-native-app/App.tsx
+++ my-first-react-native-app/App.tsx
@@ -4,8 +4,12 @@ import {
   Text, 
   View,
   Button,
+  ScrollView,
+  SafeAreaView,
  } from 'react-native';
 import {
+  ReactNode,
+  ReactElement,
   useState,
   useEffect,
 } from 'react'
@@ -20,8 +24,106 @@ export default function App() {
   //   alert('TinyTank!')
   // })
 
+  const WidthHeight = () => (
+    <View style={layoutStyles.container1}>
+        <Text>100% x 20%</Text>
+    </View>
+  )
+
+  const OuterBox = ({children}) => (
+    <View style={layoutStyles.container3}>
+      {children}
+    </View>
+  )
+
+  const InnerBox = () => (
+    <View style={layoutStyles.container2}>
+      <Text>MarginとPaddingをチェック</Text>
+    </View>
+  )
+
+  const Flexbox = () => (
+    <View style={layoutStyles.container4}>
+      <Text style={layoutStyles.item}>Item 1</Text>
+      <Text style={layoutStyles.item}>Item 2</Text>
+      <Text style={layoutStyles.item}>Item 3</Text>
+      <Text style={layoutStyles.item}>Item 4</Text>
+      <Text style={layoutStyles.item}>Item 5</Text>
+      <Text style={layoutStyles.item}>Item 6</Text>
+    </View>
+  )
+
+  const HorizontalScroll = () => (
+    <ScrollView 
+      horizontal={true} // ★水平スクロールを有効にする
+      showsHorizontalScrollIndicator={true} // スクロールバーを表示するか
+      contentContainerStyle={layoutStyles.scrollViewContent}
+    > 
+      <View style={layoutStyles.flexScrollHorizontal}>
+        {[...Array(10).map((_, i) => (
+          <Text key={i} style={layoutStyles.scrollItem}>
+              Item {i + 1}
+          </Text>
+        ))]} 
+      </View>
+    </ScrollView>
+  )
+
+  const layoutStyles = {
+    container1: {
+      width: '100%',
+      height: '20%',
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor: '#bbb',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    container2: {
+      // margin: 72,
+      marginVertical: 40,
+      marginHorizontal: 30,
+      padding: 16,
+      borderWidth: StyleSheet.hairlineWidth,
+      backgroundColor: '#ff2',
+    },
+    container3: {
+      backgroundColor: '#f2f',
+    },
+    container4: {
+      // diplay: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      // overflow: 'x-scroll',
+    },
+    item: {
+      width: 100,
+      height: 100,
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor: '#bbb',
+    },
+    scrollViewContent: {
+      widht: '100%',
+      height: '40%',
+      // alignItems: 'center'
+    }, 
+    flexScrollHorizontal: {
+      flexDirection: 'row', // 子要素を横並びにする
+      // flexWrapは指定しない (スクロールさせるため)
+      backgroundColor: '#e0f7fa',
+      padding: 10,
+      borderWidth: 1,
+      borderColor: '#00796b',
+    },
+    scrollItem: {
+      width: 100,
+      height: 100,
+      backgroundColor: '#ff9',
+      borderWidth: 1,
+    }
+  }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       {/* <Text>Open up App.tsx to start working on your app!</Text> */}
 
       {/* <Welcome name="Tiny" />
@@ -48,8 +150,23 @@ export default function App() {
         です
       </Text>
 
+      <WidthHeight />
+
+      <OuterBox>
+        <InnerBox />
+      </OuterBox>
+
+      <Flexbox />
+
+      {/* <HorizontalScroll /> */}
+
+      <View style={styles.button}>
+
+      </View>
+  
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+    
   );
 }
 
@@ -86,5 +203,13 @@ const styles = StyleSheet.create({
       { skewX: '125deg' }, // X軸方向に125°傾斜する
     ]
   },
+  button: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+    position: 'absolute',
+    right: 32,
+    bottom: 64,
+  }
 
 });
